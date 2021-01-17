b3bd21a2d4704d79af4893e577db784040704075
sintayehu1993 <sintayeh@buffalo.edu>
2021-01-16 22:54:01 -0500

    about is done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Particles from 'react-particles-js'
-import NavBar from './components/NavBar'
-import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from 'react-particles-js';
+import NavBar from './components/NavBar';
+import Header from './components/Header';
+import About from './components/About';
 function App() {
   return (
     <>
-    <Particles
-    className="particles_canvas"
-      params= {{
-        particles: {
-          number: {
-            value:40,
-            density: {
-              enable:true,
-              value_area:800
-            }
-          }
-        }
-      }}
-    />
-    <NavBar />
-    <Header />
+      <Particles
+        className="particles_canvas"
+        params={{
+          particles: {
+            number: {
+              value: 40,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+          },
+        }}
+      />
+      <NavBar />
+      <Header />
+      <About />
     </>
   );
 }
